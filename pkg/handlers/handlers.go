dcf27d3b267ae018f3742df7d8c516a121425360
KryTwo <krytwo@yahoo.com>
2022-03-28 21:53:40 +0300

    version 999

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"goServ5/pkg/structs"
 	"goServ5/repository/postgres"
+	"log"
 	"math"
 	"net/http"
 )
@@ -14,7 +15,10 @@ import (
 func GetPeoples(ctx *gin.Context) {
 
 	var json structs.Search
-	ctx.BindJSON(&json)
+	err := ctx.BindJSON(&json)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var lists []structs.People
 	var list structs.People
@@ -80,10 +84,17 @@ func GetPeoples(ctx *gin.Context) {
 
 	}
 
-	defer rows.Close()
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}() //rows.close
 
 	for rows.Next() {
-		rows.Scan(&list.ID, &list.Last_name, &list.First_name, &list.Middle_name, &list.Address)
+		if err := rows.Scan(&list.ID, &list.Last_name, &list.First_name, &list.Middle_name, &list.Address); err != nil {
+			log.Fatal(err)
+		}
 		lists = append(lists, list)
 	}
 
@@ -107,19 +118,24 @@ func GetPeoplesById(ctx *gin.Context) {
 		Join("registry AS r ON p.id = r.people_id").
 		Where("r.people_id = ?")
 
-	sql_statement, _, err := builder.ToSql()
+	stmt, _, err := builder.ToSql()
 	if err != nil {
 		fmt.Printf("%v,sql", err)
 		return
 	}
 
-	rows, err := postgres.Db.Query(sql_statement, id)
+	rows, err := postgres.Db.Query(stmt, id)
 	if err != nil {
 		fmt.Printf("%v,rows", err)
 		return
 	}
 
-	defer rows.Close()
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}() //rows.close
 
 	for rows.Next() {
 		err := rows.Scan(&list.ID, &list.Last_name, &list.First_name, &list.Middle_name, &list.Address)
@@ -139,13 +155,29 @@ func GetPeoplesById(ctx *gin.Context) {
 
 func PostPeoples(ctx *gin.Context) {
 	var newPeople structs.People
-	ctx.BindJSON(&newPeople)
+	err := ctx.BindJSON(&newPeople)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-	insertPeople := "insert into People (last_name, first_name, middle_name) VALUES ($1, $2, $3);"
-	postgres.Db.Query(insertPeople, newPeople.Last_name, newPeople.First_name, newPeople.Middle_name)
+	insertPeople := "INSERT INTO people (last_name, first_name, middle_name) VALUES ($1, $2, $3);"
+	func() {
+		_, err := postgres.Db.Query(insertPeople, newPeople.Last_name, newPeople.First_name, newPeople.Middle_name)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
 
-	insertRegistry := "insert into registry(people_id, address) values ((select max(People.id) from People),$1);"
-	postgres.Db.Query(insertRegistry, newPeople.Address)
+	insertRegistry := "INSERT INTO registry(people_id, address) VALUES ((SELECT max(People.id) FROM People),$1);"
+	func() {
+		_, err := postgres.Db.Query(insertRegistry, newPeople.Address)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
 
 	//GetPeoples(ctx)
 	ctx.IndentedJSON(http.StatusCreated, gin.H{"message": "people is added"})
@@ -153,13 +185,24 @@ func PostPeoples(ctx *gin.Context) {
 
 func ModifyPeoples(ctx *gin.Context) {
 	var changePeopleAddress structs.People
-	ctx.BindJSON(&changePeopleAddress)
+	err := ctx.BindJSON(&changePeopleAddress)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	id := changePeopleAddress.ID
 	newAddress := changePeopleAddress.Address
 
 	modifyRegistry := "UPDATE registry r SET address = $1 WHERE people_id = $2;"
-	postgres.Db.Query(modifyRegistry, newAddress, id)
+	func() {
+		_, err := postgres.Db.Query(modifyRegistry, newAddress, id)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
+
 	//GetPeoples(ctx)
 	ctx.IndentedJSON(http.StatusOK, gin.H{"message": "Address for people_id: " + id + " successfully changed"})
 
@@ -169,6 +212,12 @@ func DeletePeoplesById(ctx *gin.Context) {
 	id := ctx.Param("id")
 
 	deleteRequest := "DELETE FROM People WHERE id = $1;"
-	postgres.Db.Query(deleteRequest, id)
+	func() {
+		_, err := postgres.Db.Query(deleteRequest, id)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
 	ctx.IndentedJSON(http.StatusOK, gin.H{"message": "People is deleted"})
 }
